e65fac3053f1ffe288e643901fbf202428d85aca
LucasMazza42 <47802441+LucasMazza42@users.noreply.github.com>
2020-11-19 15:42:28 -0600

    Create UserAccount.java
    This is a class for a user. We decided that each user will have a user name and a password.

---
+++ UserAccount.java
@@ -0,0 +1,25 @@
+public class UserAccount {
+    private String userName;
+    private String password;
+
+    public UserAccount(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
