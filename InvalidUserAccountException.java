ad801b0ba7e78bfbc7e0b13a0cfd3e37e695398c
LucasMazza42 <47802441+LucasMazza42@users.noreply.github.com>
2020-11-19 16:58:08 -0600

    Create InvalidUserAccountException.java
    This is an exception that can be thrown when there is an issue with a user account that is created.

---
+++ InvalidUserAccountException.java
@@ -0,0 +1,8 @@
+public class InvalidUserAccountException extends Exception {
+    public InvalidUserAccountException(String message) {
+        super(message);
+    }
+
+    public InvalidUserAccountException() {
+    }
+}
