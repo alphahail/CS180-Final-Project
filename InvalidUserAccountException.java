852c7d42e043acd4149e8ace6f9b8fd091a20564
hreamer <reamer.hudson@gmail.com>
2020-11-20 01:14:49 -0500

    Added import statements and main method.

--- InvalidUserAccountException.java
+++ InvalidUserAccountException.java
@@ -4,5 +4,6 @@ public class InvalidUserAccountException extends Exception {
     }
 
     public InvalidUserAccountException() {
+        super();
     }
 }
--- Server.java
+++ Server.java
@@ -1,2 +1,9 @@
+import java.io.*;
+import java.net.*;
+
 public class Server {
+    public static void main (String[] args) throws UnknownHostException, IOException{
+        ServerSocket serverSocket = new ServerSocket(4242);
+        
+    }
 }
