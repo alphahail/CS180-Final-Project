a574179987f730b996585c7981bbdfa478e9fb13
LucasMazza42 <47802441+LucasMazza42@users.noreply.github.com>
2020-11-19 20:29:50 -0600

    Update UserAccount.java

--- UserAccount.java
+++ UserAccount.java
@@ -22,4 +22,12 @@ public class UserAccount {
     public void setPassword(String password) {
         this.password = password;
     }
+    
+    @Override
+    public String toString() {
+        return "UserAccount{" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
