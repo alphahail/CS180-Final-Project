02d305a6695000300d3d2f0d22a51b6fc9aa91b9
hreamer <reamer.hudson@gmail.com>
2020-11-22 23:26:09 -0500

    Saving changes before pulling to latest code

--- ClientThread.java
+++ ClientThread.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.net.Socket;
 
 public class ClientThread extends Thread{
@@ -11,5 +12,33 @@ public class ClientThread extends Thread{
     public void run() {
         /*Here will go the implementation for
         * Each Client  */
+
+    }
+
+    public String isValidLogin(String userName, String password) throws IOException {
+        File f = new File("users.txt");
+        FileReader fos = new FileReader(f);
+        BufferedReader br = new BufferedReader(fos);
+
+        String user = br.readLine();
+        String tempPass = br.readLine();
+        while(user != null && tempPass != null) {
+            if (user.equals(userName) && tempPass.equals(password)) {
+                return "Valid User";
+            }
+
+            user = br.readLine();
+            password = br.readLine();
+        }
+        return "Invalid User";
+    }
+
+    public void addUser(String userName, String password) throws FileNotFoundException {
+        File f = new File("users.txt");
+        FileOutputStream fos = new FileOutputStream(f, true);
+        PrintWriter pw = new PrintWriter(fos);
+
+        pw.println(userName);
+        pw.println(password);
     }
 }
